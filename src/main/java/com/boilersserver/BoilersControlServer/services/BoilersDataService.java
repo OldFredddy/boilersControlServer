a231b6fb2fa86d62b7974d6f7fb9b244e92f71bb
OldFredddy <126678451+OldFredddy@users.noreply.github.com>
2024-04-02 08:11:30 +0300

    -nullPointerException

--- src/main/java/com/boilersserver/BoilersControlServer/services/BoilersDataService.java
+++ src/main/java/com/boilersserver/BoilersControlServer/services/BoilersDataService.java
@@ -65,6 +65,13 @@ public class BoilersDataService {
         this.connectionProvider.dispose();
         System.out.println("clean BoilersDataService");
     }
+    public String getCorrectionTpodAtIndex(int index) {
+        String[] corrections = getCorrections().getCorrectionTpod();
+        if (corrections == null || corrections.length <= index || corrections[index] == null) {
+            return "0"; 
+        }
+        return corrections[index];
+    }
     public boolean forFirstStart = true;
     @Scheduled(fixedRate = 3000)
     public void fetchBoilerData() {
