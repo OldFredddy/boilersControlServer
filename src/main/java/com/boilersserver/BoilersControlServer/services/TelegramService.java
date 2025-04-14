75a53a5942007a619a38c07f1c31729e13a41e05
OldFredddy <126678451+OldFredddy@users.noreply.github.com>
2025-04-14 08:01:34 +0300

    +update boiler5 error

--- src/main/java/com/boilersserver/BoilersControlServer/services/TelegramService.java
+++ src/main/java/com/boilersserver/BoilersControlServer/services/TelegramService.java
@@ -257,6 +257,9 @@ public class TelegramService extends TelegramLongPollingBot {
                     if (pressureErrorsArray[i]){
                         continue;
                     }
+                    if (i==5){
+                        continue;
+                    }
                     if (boilersDataService.getBoilers().get(i).getPPodLowFixed().equals("-1.0")||boilersDataService.getBoilers().get(i).getPPodHighFixed().equals("-1.0")){
                         currentPpodHigh=normalPvxHigh[i];
                         currentPpodLow=normalPvxLow[i];
